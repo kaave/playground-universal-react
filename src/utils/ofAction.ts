c16453f50ae62abbff82a409319f1ea4e1266401
kaave <abe@framelunch.jp>
2018-11-05 18:17:34 +0900

    common: remove unuse files

--- src/utils/ofAction.ts
+++
@@ -1,9 +0,0 @@
-// https://github.com/m0a/typescript-fsa-redux-observable/issues/6
-
-import { filter } from 'rxjs/operators';
-import { Action, ActionCreator } from 'typescript-fsa';
-import { Observable, OperatorFunction } from 'rxjs';
-
-export const ofAction = <P>(actionCreator: ActionCreator<P>): OperatorFunction<Action<unknown>, Action<P>> => (
-  actions$,
-): Observable<Action<P>> => actions$.pipe(filter(actionCreator.match));

ae39cdd9bbd91e26255497d15f413107f7d6b706
kaave <abe@framelunch.jp>
2018-11-07 14:49:50 +0900

    modal: add simple modal

---
+++ src/components/organisms/modal/index.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import ReactModal, { Styles } from 'react-modal';
+
+export interface Props {
+  isOpen: boolean;
+  onCloseClick: () => void;
+}
+
+const styles: Styles = {
+  content: {
+    position: 'absolute',
+    display: 'block',
+    top: '20%',
+    left: '20%',
+    width: '60%',
+    height: '60%',
+    background: '#fff',
+    color: '#000',
+  },
+  overlay: {
+    background: 'rgba(0, 0, 0, 0.3)',
+  },
+};
+
+export class Modal extends React.Component<Props, {}> {
+  componentDidMount() {
+    if (document) {
+      const mountPoint = document.getElementById('mount-point');
+
+      if (mountPoint) {
+        ReactModal.setAppElement(mountPoint);
+      }
+    }
+  }
+
+  render() {
+    const { isOpen, onCloseClick } = this.props;
+
+    return (
+      <ReactModal isOpen={isOpen} style={styles}>
+        Modal
+        <button onClick={onCloseClick}>×</button>
+      </ReactModal>
+    );
+  }
+}
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Switch, Link } from 'react-router-dom';
-import { TransitionGroup, CSSTransition, Transition } from 'react-transition-group';
-
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import format from 'date-fns/format';
 import { renderRoutes, RouteConfigComponentProps } from 'react-router-config';
 import { ConnectedRouterProps } from 'connected-react-router';
@@ -11,6 +10,7 @@ import { returntypeof } from 'react-redux-typescript';
 
 import { State } from '../reduxes/reducers';
 import { mapDispatchToProps } from '../reduxes/actions';
+import { Modal } from '../components/organisms/modal';
 
 function mapStateToProps(state: State) {
   return { ...state };
@@ -28,50 +28,60 @@ type AppProps = typeof stateProps & typeof dispatchProps & RouteConfigComponentP
 
 export interface AppState {
   counter: number;
+  isModalOpen: boolean;
 }
 
-export default connectToStore(
-  class App extends React.Component<AppProps, AppState> {
-    state = { counter: 0 };
+class App extends React.Component<AppProps, AppState> {
+  state = { counter: 0, isModalOpen: false };
 
-    componentDidMount() {
-      this.startCountup();
-    }
+  componentDidMount() {
+    this.startCountup();
+  }
 
-    startCountup() {
-      setInterval(() => this.setState({ ...this.state, counter: this.state.counter + 1 }), 1000);
-    }
+  startCountup() {
+    setInterval(() => this.setState({ ...this.state, counter: this.state.counter + 1 }), 1000);
+  }
 
-    increment = () => this.props.increment();
-    decrement = () => this.props.decrement();
-    asyncIncrement = () => this.props.asyncIncrement();
-    asyncDecrement = () => this.props.asyncDecrement();
+  increment = () => this.props.increment();
+  decrement = () => this.props.decrement();
+  asyncIncrement = () => this.props.asyncIncrement();
+  asyncDecrement = () => this.props.asyncDecrement();
 
-    render() {
-      const { counter } = this.state;
-      const currentKey = this.props.location.pathname.split('/')[1] || '/';
+  handleOpenModalClick = () => this.setState({ isModalOpen: true });
+  handleCloseModalClick = () => this.setState({ isModalOpen: false });
 
-      return (
-        <main id="main" className="Main" role="main">
-          <Helmet>
-            <title>base title</title>
-          </Helmet>
-          <Link to="/">to index</Link>
-          <Link to="/demo">to demo</Link>
-          now: {format(new Date())}, auto counter: {counter}, storeCounter: {this.props.count}
-          <TransitionGroup>
-            <CSSTransition key={currentKey} classNames="fade" timeout={5000}>
-              <Switch location={this.props.location}>
-                {this.props.route && renderRoutes(this.props.route.routes)}
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-          <button onClick={this.increment}>increment</button>
-          <button onClick={this.decrement}>decrement</button>
-          <button onClick={this.asyncIncrement}>async increment</button>
-          <button onClick={this.asyncDecrement}>async decrement</button>
-        </main>
-      );
-    }
-  },
-);
+  render() {
+    const { counter, isModalOpen } = this.state;
+    const currentKey = this.props.location.pathname.split('/')[1] || '/';
+
+    return (
+      <main id="main" className="Main" role="main">
+        <Helmet>
+          <title>base title</title>
+        </Helmet>
+        <Modal isOpen={isModalOpen} onCloseClick={this.handleCloseModalClick} />
+        <ul className="LinkList">
+          <li className="LinkList__cell">
+            <Link to="/">to index</Link>
+          </li>
+          <li className="LinkList__cell">
+            <Link to="/demo">to demo</Link>
+          </li>
+        </ul>
+        now: {format(new Date())}, auto counter: {counter}, storeCounter: {this.props.count}
+        <TransitionGroup>
+          <CSSTransition key={currentKey} classNames="fade" timeout={5000}>
+            <Switch location={this.props.location}>{this.props.route && renderRoutes(this.props.route.routes)}</Switch>
+          </CSSTransition>
+        </TransitionGroup>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+        <button onClick={this.asyncIncrement}>async increment</button>
+        <button onClick={this.asyncDecrement}>async decrement</button>
+        <button onClick={this.handleOpenModalClick}>Open modal</button>
+      </main>
+    );
+  }
+}
+
+export default connectToStore(App);

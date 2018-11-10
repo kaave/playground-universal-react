8d864133aa26aeb155d4d48e58dd61b3257fad0c
kaave <abe@framelunch.jp>
2018-11-10 23:44:42 +0900

    express: add parsers

--- src/server/http.ts
+++ src/server/http.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
-import { Express } from 'express';
 import compression from 'compression';
 import winston from 'winston';
 import expressWinston from 'express-winston';
 import format from 'date-fns/format';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
 
 import { setApiRoutes } from './api';
 import router from './routes/';
@@ -15,7 +17,7 @@ const logFormat = winston.format.printf(
   info => `${format(new Date(), 'YYYY-MM-DD HH:mm:ss.SSS')} ${info.level}: ${info.meta.res.statusCode} ${info.message}`,
 );
 
-async function initializeDevServer(expressApp: Express) {
+async function initializeDevServer(expressApp: express.Express) {
   const { default: webpack } = await import('webpack');
   const { default: webpackHotMiddleware } = await import('webpack-hot-middleware');
   const { default: webpackDevMiddleware } = await import('webpack-dev-middleware');
@@ -56,6 +58,25 @@ async function main() {
       }),
     );
   }
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use(
+    expressSession({
+      secret: process.env.SESSION_SECRET_KEY || '__SET_SESSION_SECRET_KEY_TO_DOTENV__',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { httpOnly: !isDevelopment, maxAge: parseInt(process.env.COOKIE_MAX_AGE || '', 10) || 24 * 60 * 60 },
+      // save server session info to redis
+      // store: new RedisStore({
+      //   host: config.redis.host,
+      //   port: config.redis.port,
+      //   db: config.redis.db,
+      //   pass: config.redis.pass
+      // }),
+    }),
+  );
 
   app.set('view engine', 'ejs');
   app.set('views', 'src/views');

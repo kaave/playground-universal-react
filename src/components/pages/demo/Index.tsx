90e6349758711455e3b7f2b935b482ecd78e1957
kaave <abe@framelunch.jp>
2019-03-17 19:40:50 +0900

    demo: re-use react-css-modules

--- src/components/pages/demo/Index.tsx
+++ src/components/pages/demo/Index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-// import CSSModules from '~/utils/cssModules';
-// import styles from './style.css';
+import CSSModules from '~/utils/cssModules';
+import styles from './style.css';
 import * as Meta from '~/value-objects/meta';
 import { updateMetaService } from '~/services/updateMetaService';
 
@@ -10,18 +10,14 @@ export const meta: Meta.Meta = Meta.create({
   description: 'demo page description',
 });
 
-export default class DemoPage extends React.Component {
-  componentDidMount() {
-    updateMetaService(meta);
-  }
+const DemoPage: React.FC<{}> = () => {
+  React.useEffect(() => updateMetaService(meta), []);
 
-  render() {
-    return (
-      <section styleName="Demo" className="Demo">
-        demo
-      </section>
-    );
-  }
-}
+  return (
+    <section styleName="Demo" className="Demo">
+      demo
+    </section>
+  );
+};
 
-// export default CSSModules(DemoPage, styles) as () => JSX.Element;
+export default CSSModules(DemoPage, styles) as () => JSX.Element;
